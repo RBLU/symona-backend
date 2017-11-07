df7f32ca1c36c9e23fb92c9c22782febb1baba26
RBLU <reto.blunschi@youpers.com>
2017-11-07 10:47:34 +0100

    fix cors, added more routes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const corsMiddleware = require('restify-cors-middleware');
 const restify = require('restify');
 const pino = require('restify-pino-logger')();
 const dotenv = require('dotenv').config();
-const db  = require('./db');
+const db = require('./db');
 const passport = require('passport');
 
 const config = require('../config/config');
@@ -14,10 +14,10 @@ const handler = require('./handler');
 
 
 const cors = corsMiddleware({
-  preflightMaxAge: 5, //Optional
-  origins: ['http://api.myapp.com', 'http://web.myapp.com'],
-  allowHeaders: ['API-Token'],
-  exposeHeaders: ['API-Token-Expiry']
+    preflightMaxAge: 5, //Optional
+    origins: ['http://localhost:4200', 'https://symona.youpers.com'],
+    allowHeaders: ['API-Token'],
+    exposeHeaders: ['API-Token-Expiry']
 });
 
 const server = restify.createServer();
@@ -27,7 +27,8 @@ server.pre(cors.preflight);
 server.use(cors.actual);
 server.use(pino);
 server.use(restify.plugins.acceptParser(server.acceptable));
-server.use(restify.plugins.queryParser({ mapParams: false }));
+server.use(restify.plugins.queryParser({mapParams: false}));
+
 server.use(restify.plugins.bodyParser({mapParams: false}));
 server.use(passport.initialize());
 
@@ -36,8 +37,9 @@ handler.addRoute(server, require('./routes/monitoring'));
 handler.addRoute(server, require('./routes/target'));
 handler.addRoute(server, require('./routes/inspectiondef'));
 handler.addRoute(server, require('./routes/inspection'));
+handler.addRoute(server, require('./routes/run'));
 
-server.listen(8080, function() {
-  console.log('%s listening at %s', server.name, server.url);
+server.listen(8080, function () {
+    console.log('%s listening at %s', server.name, server.url);
 });
 
---
+++ src/routes/run.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+    table: 'Run',
+    primaryKey: 'boid'
+};
