e93f03e6b8c36bc3263d2d454fb1223f7f670c7d
RBLU <reto.blunschi@youpers.com>
2017-11-17 11:21:17 +0100

    install oracle client

--- scripts/node-oracledb-prepare.sh
+++ scripts/node-oracledb-prepare.sh
@@ -2,16 +2,16 @@
 set -e
 
 
-if [ -e  /home/circleci/.oracle/instantclient/libclntsh.so ]; then
+if [ -e  /home/circleci/repo/.oracle/instantclient/libclntsh.so ]; then
     echo oracle instant client: using cached version
 else
-    mkdir -p ~/.oracle
-    cd ~/.oracle
+    mkdir -p ~/repo/.oracle
+    cd ~/repo/.oracle
     wget --no-check-certificate https://symona.youpers.com/oracleclient/instantclient-basiclite-linux.x64-12.1.0.2.0.zip
     wget --no-check-certificate https://symona.youpers.com/oracleclient/instantclient-sdk-linux.x64-12.1.0.2.0.zip
     unzip instantclient-basiclite-linux.x64-12.1.0.2.0.zip
     unzip instantclient-sdk-linux.x64-12.1.0.2.0.zip
     mv instantclient_12_1 instantclient
-    cp ~/.oracle/instantclient/libocci.so.12.1 ~/.oracle/instantclient/libocci.so
-    cp ~/.oracle/instantclient/libclntsh.so.12.1 ~/.oracle/instantclient/libclntsh.so
+    cp ~/repo/.oracle/instantclient/libocci.so.12.1 ~/repo/.oracle/instantclient/libocci.so
+    cp ~/repo/.oracle/instantclient/libclntsh.so.12.1 ~/repo/.oracle/instantclient/libclntsh.so
 fi
