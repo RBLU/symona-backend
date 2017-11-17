813fa294fbf2dbff60ed13ef804cadf921654b46
RBLU <reto.blunschi@youpers.com>
2017-11-17 10:29:16 +0100

    install oracle client

---
+++ scripts/node-oracledb-prepare.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+
+if [ -f  ~/.oracle/instantclient/libclntsh.so ]; then
+    echo oracle instant client: using cached version
+else
+    mkdir -p ~/.oracle
+    cd ~/.oracle
+    wget --no-check-certificate https://symona.youpers.com/oracleclient/instantclient-basiclite-linux.x64-12.1.0.2.0.zip
+    wget --no-check-certificate https://symona.youpers.com/oracleclient/instantclient-sdk-linux.x64-12.1.0.2.0.zip
+    unzip instantclient-basiclite-linux.x64-12.1.0.2.0.zip
+    unzip instantclient-sdk-linux.x64-12.1.0.2.0.zip
+    mv instantclient_12_1 instantclient
+    cp ~/.oracle/instantclient/libocci.so.12.1 ~/.oracle/instantclient/libocci.so
+    cp ~/.oracle/instantclient/libclntsh.so.12.1 ~/.oracle/instantclient/libclntsh.so
+fi
